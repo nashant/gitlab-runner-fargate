39ec6bba3045ee1d4e8f6e7e6666dd7143e6a849
Anthony Nash <anthony.nash@du.co>
2020-11-11 17:05:15 +0000

    Missing space

--- run.sh
+++ run.sh
@@ -69,7 +69,7 @@ push_token() {
 let VALIDATE_COUNT=0
 while [[ "$(validate_runner_token)" != "200" ]]; do
   [ $VALIDATE_COUNT -eq 10 ] && error "Unable to register runner"
-  [[ "$RUNNER_TOKEN" == "REPLACE_ME"]] || echo "Token invalid"
+  [[ "$RUNNER_TOKEN" == "REPLACE_ME" ]] || echo "Token invalid"
   register_runner
   sleep 10
   let VALIDATE_COUNT++
