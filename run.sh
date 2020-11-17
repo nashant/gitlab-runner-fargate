62c7feb4d7b97fbef9b97a7db28ea366f2234ed2
Anthony Nash <anthony.nash@du.co>
2020-11-17 15:32:16 +0000

    Try to propagate AWS vars

--- run.sh
+++ run.sh
@@ -10,35 +10,19 @@ error() {
   exit 1
 }
 
-VARS="PRIVATE_KEY URL REGISTRATION_TOKEN RUNNER_TOKEN RUNNER_TOKEN_SSM_PARAMETER NAME TAGS CLUSTER REGION SUBNET SECURITYGROUP TASK"
-
 verify_var() {
   VAR="$1"
   [ -n "${!VAR}" ] || error "$VAR is a mandatory env var"
 }
 
-for VAR in $VARS; do
-  verify_var "$VAR"
-done
-
-until curl "$URL" -s -o /dev/null; do
-  stderr "Trying to curl $URL"
-  sleep 5
-done
-
-stderr "Waiting for $URL to become ready"
-let CHECK_COUNT=0
-while [[ "$STATUS" != "ok" ]]; do
-  if [ "$CHECK_COUNT" -eq 10 ]; then
-    stderr "Unable to get readiness of $URL"
-    exit 1
-  fi
-  STATUS=$(curl -s "$URL/-/readiness" | jq -r '.master_check[0].status')
-  [[ "$STATUS" == "ok" ]] && continue
-  stderr "Status is $STATUS. Waiting 30s"
-  sleep 30
-  let CHECK_COUNT++
-done
+propagate_aws_env_vars() {
+  AWS_ENV_VARS=$(env | grep 'AWS_\|ECS_')
+  SET_AWS_ENV_VARS_SCRIPT=/etc/profile.d/set-aws-env-vars.sh
+  echo > $SET_AWS_ENV_VARS_SCRIPT
+  for VARIABLE in $AWS_ENV_VARS; do
+    echo "export $VARIABLE" >> $SET_AWS_ENV_VARS_SCRIPT
+  done
+}
 
 register_runner() {
   stderr "Registering runner"
@@ -56,7 +40,34 @@ push_token() {
   aws ssm put-parameter --region "$REGION" --name "$RUNNER_TOKEN_SSM_PARAMETER" --value "$RUNNER_TOKEN" --type SecureString --overwrite
 }
 
-let VALIDATE_COUNT=0
+VARS="PRIVATE_KEY URL REGISTRATION_TOKEN RUNNER_TOKEN RUNNER_TOKEN_SSM_PARAMETER NAME TAGS CLUSTER REGION SUBNET SECURITYGROUP TASK"
+
+for VAR in $VARS; do
+  verify_var "$VAR"
+done
+
+until curl "$URL" -s -o /dev/null; do
+  stderr "Trying to curl $URL"
+  sleep 5
+done
+
+stderr "Waiting for $URL to become ready"
+CHECK_COUNT=0
+while [[ "$STATUS" != "ok" ]]; do
+  if [ "$CHECK_COUNT" -eq 10 ]; then
+    stderr "Unable to get readiness of $URL"
+    exit 1
+  fi
+  STATUS=$(curl -s "$URL/-/readiness" | jq -r '.master_check[0].status')
+  [[ "$STATUS" == "ok" ]] && continue
+  stderr "Status is $STATUS. Waiting 30s"
+  sleep 30
+  (( CHECK_COUNT++ ))
+done
+
+propagate_aws_env_vars
+
+VALIDATE_COUNT=0
 while true; do
   [ $VALIDATE_COUNT -eq 10 ] && error "Unable to register runner"
   VALIDATION=$(validate_runner_token)
@@ -64,7 +75,7 @@ while true; do
   [[ "$RUNNER_TOKEN" == "REPLACE_ME" ]] || stderr "Token invalid: $VALIDATION"
   register_runner
   sleep 10
-  let VALIDATE_COUNT++
+  (( VALIDATE_COUNT++ ))
 done
 
 cp -r /opt/gitlab-runner/* /etc/gitlab-runner/
